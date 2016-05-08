8959e56cb48407f8043680c2c79b5137d7b12b6a
robynml <robynml@users.noreply.github.com>
2016-05-09 00:36:01 +0200

    Update head.php
    Add missing viewport info

--- include/head.php
+++ include/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html" />
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title><?php echo $language; ?> Dictionary</title>
     <link rel="stylesheet" href="include/bootstrap.min.css" >
     <link rel="stylesheet" href="stylesheets/lexiquepro.css" type="text/css" />
