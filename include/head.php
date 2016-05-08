106644bdb44b2770d9c06931f393be462a2f4fac
robynml <robynml@users.noreply.github.com>
2016-05-08 23:15:57 +0200

    Add files via upload
    initial commit

---
+++ include/head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta http-equiv="content-type" content="text/html" />
+    <meta charset="UTF-8">
+    <title><?php echo $language; ?> Dictionary</title>
+    <link rel="stylesheet" href="include/bootstrap.min.css" >
+    <link rel="stylesheet" href="stylesheets/lexiquepro.css" type="text/css" />
+    <link rel="stylesheet" href="include/styles.css" >
+</head>
---
+++ include/script.php
@@ -0,0 +1,3 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="include/bootstrap.min.js"></script>
+<script src="include/scripts.js"></script>
